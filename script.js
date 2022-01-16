9817458280d353f8faa9976e13a044bff6645cd1
Bivar Dourado <bivardourado/2gmail.com>
2022-01-15 21:58:25 -0300

    Inserindo funções pra gerar as 2 imagens

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const imagem3 = document.querySelector('#imagem3');
 const nome3 = document.querySelector('#nome3');
 const especie3 = document.querySelector('#especie3');
 const condicao3 = document.querySelector('#status3');
+
+
+traduzirCondicao = (data) => {
+    if (data.status == 'unknown') {
+        return 'Não sabemos';
+    } else if (data.status == 'Alive') {
+        return 'Sim';
+    } else {
+        return 'Não. Está morto';
+    }
+}
+
+gerarValorAletorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+        let numeroAleatorio = gerarValorAletorio();
+        let numeroAleatorio2 = gerarValorAletorio();
+        let numeroAleatorio3 = gerarValorAletorio();
