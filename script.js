5842b2be71387dda793425929f9b21b7b2bd3f24
Bivar Dourado <bivardourado/2gmail.com>
2022-01-15 21:17:07 -0300

    capturar elementos html manipulaveis da  terceira figura

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const botao = document.querySelector('button');
 const nome = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+const imagem2 = document.querySelector('#imagem2');
+const nome2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+
+const imagem3 = document.querySelector('#imagem3');
+const nome3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
